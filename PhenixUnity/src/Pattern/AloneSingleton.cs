fbcb508a429f0915695c8e1804d81362b1898a94
phenix1021 <phenix1021@gmail.com>
2019-09-22 23:23:30 +0800

    log修改

---
+++ PhenixUnity/src/Pattern/AloneSingleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Phenix.Unity.Pattern
+{
+    /// <summary>
+    /// 独立单件模式（不需要显式挂接对象，但无法在设计期序列化字段）
+    /// </summary>
+    public abstract class AloneSingleton<T> : MonoBehaviour where T : AloneSingleton<T>
+    {
+        static T _inst;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_inst == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name, typeof(T)) { hideFlags = HideFlags.HideInHierarchy };
+                    DontDestroyOnLoad(obj);
+                    _inst = obj.GetComponent<T>();                    
+                }
+                return _inst;
+            }
+        }
+    }
+}
--- PhenixUnity/src/Pattern/Singleton.cs
+++ PhenixUnity/src/Pattern/Singleton.cs
@@ -5,24 +5,14 @@ namespace Phenix.Unity.Pattern
     /// <summary>
     /// 单件模式
     /// </summary>
-    /// <typeparam name="T"></typeparam>
     /// <remark>派生类继承该类时需要调用base.Awake()或不写Awake函数</remark>>
     public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-        static T _inst;
+        public static T Instance { get; private set; }
 
-        public static T Instance
+        protected virtual void Awake()
         {
-            get
-            {
-                if (_inst == null)
-                {
-                    GameObject obj = new GameObject(typeof(T).Name, typeof(T)) { hideFlags = HideFlags.HideInHierarchy };
-                    DontDestroyOnLoad(obj);
-                    _inst = obj.GetComponent<T>();                    
-                }
-                return _inst;
-            }
+            Instance = (T)this;
         }
     }
 }
