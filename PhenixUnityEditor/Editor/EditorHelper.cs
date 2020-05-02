86cb627074b2db1d643771d223f5715f69f2f051
phenix1021 <phenix1021@gmail.com>
2020-05-02 20:27:06 +0800

    1.GUI控件库 2.辅助类 3.路径管理器inspector

---
+++ PhenixUnityEditor/Editor/EditorHelper.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Phenix.Unity.Editor.Utilities
+{
+
+    public static class EditorHelper
+    {
+        /// <summary>
+        /// 游戏是否正在play
+        /// </summary>
+        public static bool IsPlaying { get { return EditorApplication.isPlaying; } }
+
+        /// <summary>
+        /// 给资源（如Texture2D、ScriptObject等）打flag，标记其数据改变，需要存储到硬盘
+        /// </summary>
+        public static void SetAssetDirty(UnityEngine.Object asset)
+        {
+            EditorUtility.SetDirty(asset);
+        }
+
+        /// <summary>
+        /// 给当前场景打标记，说明其数据改变，需要存储
+        /// </summary>
+        public static void SetSceneDirty()
+        {            
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(
+                UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+        }
+
+        /// <summary>
+        /// Pulls variables from runtime so we have the latest values.
+        /// 常用在Editor.OnInspectorGUI()的开始
+        /// </summary>
+        public static void UpdateValue(SerializedObject so)
+        {
+            so.Update();
+        }
+
+        /// <summary>
+        /// Inspector中获得指定成员的SerializedProperty
+        /// 对于基本类型的成员可以通过IntField、FloatField来访问，
+        /// 对于自定义类型成员就只能通过本方法然后再由PropertyField访问
+        /// </summary>
+        /// <param name="so">如Inspector中的serializedObject</param>
+        /// <param name="memberName">成员变量名</param>
+        public static SerializedProperty FindSP(SerializedObject so, string memberName)
+        {
+            return so.FindProperty(memberName);
+        }
+
+        /// <summary>
+        /// Pushes the values back to the runtime so it has the changes
+        /// 常用在Editor.OnInspectorGUI()的结尾
+        /// </summary>
+        public static void Submit(SerializedObject so)
+        {
+            so.ApplyModifiedProperties();
+        }        
+
+        /// <summary>
+        /// 在scene窗口绘制点
+        /// </summary>
+        /// <param name="pos">点的位置</param>        
+        /// <param name="color">点的颜色</param>        
+        /// 返回是否选中
+        public static bool Dot(Vector3 pos, Color color)
+        {            
+            Handles.color = color;            
+            float handleSize = HandleUtility.GetHandleSize(pos);
+            // 创建交互点
+            return Handles.Button(pos, Quaternion.identity, 0.06f * handleSize, 
+                0.08f * handleSize, Handles.DotHandleCap);            
+        }
+
+        /// <summary>
+        /// 在scene窗口指定位置添加移动轴（xyz轴）
+        /// </summary>
+        /// <param name="pos"></param>
+        public static void AddMoveAxis(ref Vector3 pos)
+        {
+            pos = Handles.DoPositionHandle(pos, Quaternion.identity);
+        }
+    }
+}
---
+++ PhenixUnityEditor/Editor/Inspectors/PathMgrInspector.cs
@@ -0,0 +1,93 @@
+﻿using UnityEditor;
+using Phenix.Unity.Movement;
+using Phenix.Unity.Editor.Utilities;
+using Phenix.Unity.Editor.Control;
+using UnityEngine;
+using System.Collections.Generic;
+using Phenix.Unity.Utilities;
+
+namespace Phenix.Unity.Editor.Inspector
+{
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(PathMgr))]
+    public class PathMgrInspector : UnityEditor.Editor
+    {
+        PathMgr _pathMgr;
+        bool _switchForPath = true;     // 路径点foldout是否展开
+        int _curPathPointSelected = -1;  // 当前选中的路径点
+
+        List<Vector3> _fullPathPoints = new List<Vector3>();
+
+        private void OnEnable()
+        {
+            _pathMgr = (PathMgr)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // 同步最新值
+            EditorHelper.UpdateValue(serializedObject);
+
+            // 是否循环
+            EditorControl.BoolField("loop:", "", ref _pathMgr.loop);
+
+            EditorControl.Foldout("Path:", "", ref _switchForPath);
+            if (_switchForPath)
+            {
+                // 各个路径点
+                for (int i = 0; i < _pathMgr.points.Count; ++i)
+                {
+                    _pathMgr.points[i] = EditorControl.Vector3Field(string.Format("point {0}:", i), "", _pathMgr.points[i]);
+                }
+            }           
+
+            if (EditorControl.Button("Add Point", ""))
+            {                
+                _pathMgr.AddPoint(_pathMgr.transform.position + Random.onUnitSphere);                             
+            }
+
+            if (EditorControl.Button("Remove Point", ""))
+            {
+                _pathMgr.RemovePoint(_curPathPointSelected);
+                _curPathPointSelected = _pathMgr.points.Count-1;
+            }
+
+            if (EditorControl.Button("Clear All", ""))
+            {
+                _pathMgr.Clear();
+                _curPathPointSelected = -1;
+            }
+
+            // 提交改变
+            EditorHelper.Submit(serializedObject);
+        }
+
+        private void OnSceneGUI()
+        {
+            for (int i = 0; i < _pathMgr.points.Count; ++i)
+            {
+                Vector3 pos = _pathMgr.transform.TransformPoint(_pathMgr.points[i]);
+                bool selected = (_curPathPointSelected == i);
+                if (selected)
+                {
+                    EditorHelper.AddMoveAxis(ref pos);
+                }
+                if (EditorHelper.Dot(pos, Color.white))
+                {                    
+                    _curPathPointSelected = i;                    
+                }                
+                _pathMgr.points[i] = _pathMgr.transform.InverseTransformPoint(pos);
+            }
+
+            MathTools.GetCatmullRomSplineFullPathPoints(_pathMgr.points, _pathMgr.loop, ref _fullPathPoints);
+            Handles.color = Color.yellow;
+            for (int i = 0; i < _fullPathPoints.Count-1; i++)
+            {
+                Vector3 from = _pathMgr.transform.TransformPoint(_fullPathPoints[i]);
+                Vector3 to = _pathMgr.transform.TransformPoint(_fullPathPoints[i+1]);                
+                Handles.DrawLine(from, to);
+            }
+        }
+    }
+}
